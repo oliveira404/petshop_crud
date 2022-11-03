8231121cee45c3a8ae4e9781af86a1d851059051
oliveiradeigor <lrmcpe22@gmail.com>
2022-11-02 21:38:55 -0300

    criando classe de conexão com pool de conexões

---
+++ src/main/java/factory/ConnectionFactory.java
@@ -0,0 +1,41 @@
+package factory;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public DataSource dataSource; //alternativa ao Driver manager
+
+    public ConnectionFactory(){
+        ComboPooledDataSource comboPool = new ComboPooledDataSource();
+
+        //Se for banco MariaDB usar esse.
+        comboPool.setJdbcUrl("jdbc:mariadb://localhost/petshop_crud?useTimezone=true&serverTimezone=UTC");
+
+        //Se for Mysql usar esse
+        //comboPool.setJdbcUrl("jdbc:mysql://localhost/petshop_crud?useTimezone=true&serverTimezone=UTC");
+
+        //login e senha do seu banco de dados
+        comboPool.setUser("igor");
+        comboPool.setPassword("0122");
+
+        //limites de conexões ao mesmo tempo
+        comboPool.setMaxPoolSize(30);
+
+        //passando os dados de conexões para o jdbc
+        this.dataSource = comboPool;
+    }
+
+    public Connection recoveryConnection(){
+        try{
+            return this.dataSource.getConnection();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
