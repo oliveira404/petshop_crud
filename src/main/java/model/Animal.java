018b9e65de5c74d38e231a2ff328c7d3ecb725d2
oliveiradeigor <lrmcpe22@gmail.com>
2022-11-03 21:34:56 -0300

    criação das duas classes animal e cliente

---
+++ src/main/java/model/Animal.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Animal {
+    private Integer id;
+    private String nome;
+    private String animal;
+    private String raca;
+    private Integer idade;
+    private Integer fkCliente;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Integer getFkCliente() {
+        return fkCliente;
+    }
+
+    public void setFkCliente(Integer fkCliente) {
+        this.fkCliente = fkCliente;
+    }
+}
